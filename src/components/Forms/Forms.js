272faa2d54368e4e65d5e427ca22f990f7b893ba
Archna Kumari <archnakumari310@gmail.com>
2021-01-24 19:40:52 +0530

    added form component

--- src/components/Forms/Forms.js
+++ src/components/Forms/Forms.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from "react";
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import InputForm from './FormValidation'
 
 export default function Forms() {
-    return (
-        <Container maxWidth="lg">
-        <Typography component="div" variant="h4" style={{ backgroundColor: '#fff', height: '100vh', marginTop: '120px' }} > 
+  return (
+    <Container maxWidth="lg">
+      <Typography component="div" variant="h4" style={{ backgroundColor: '#fff', height: '100vh', marginTop: '120px' }} >
         Forms Component goes here
-         </Typography>
-      </Container>
-    )
+        <InputForm />
+      </Typography>
+    </Container>
+  )
 }
+
